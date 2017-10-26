b0aec530b491fc2f8d55b8b9bd6bef4cc698d07e
Kalle Lilja <desethsc@gmail.com>
2017-10-26 18:25:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,38 +5,39 @@ IFS=$'\n\t'
 # Description:       Deploy Mersenne mprime software as a daemon.
 #                    New user account created: mprime
 #                    New files:
-#                    /etc/init.d/mprime.
-#                    /usr/sbin/mprime/*.
-#                    /var/log/mprime.log
-#                    /etc/logrotate.d/mprime
-# Usage:             sudo /etc/init.d/mprime {start|stop|restart|force-reload}
+#                    /etc/init.d/mprimed
+#                    /usr/sbin/mprime/*
+#                    /var/log/mprimed.log
+#                    /etc/logrotate.d/mprimed
+# Usage:             sudo /etc/init.d/mprimed {start|stop|restart|force-reload}
 #                    tail -f /var/log/mprime.log
+#                    tail -f /usr/sbin/mprime/prime.log
 #                    /usr/sbin/mprime/mprime -s
 
 # Author: Kalle Lilja
 #
 
-# Setup Structure
+# Setup structure
 sudo useradd -r mprime
 sudo mkdir /usr/sbin/mprime
 
-# Setup Run Script
+# Setup run script
 sudo cp ./startmprime /usr/sbin/mprime/startmprime
 sudo chmod a+x /usr/sbin/mprime/startmprime
 sudo chown mprime:mprime /usr/sbin/mprime /usr/sbin/mprime/startmprime
 
-# Setup Logging
+# Setup logging
 sudo touch /var/log/mprime.log
 sudo chown mprime:mprime /var/log/mprime.log
 # Logrotate
-sudo touch /etc/logrotate.d/mprime
-echo -e '/var/log/mprime.log {\nrotate7\ndaily\nmissingok\nnotifempty\ncompress\ndelaycompress\ncopytruncate\n}' | sudo tee -a /etc/logrotate.d/mprime
+sudo touch /etc/logrotate.d/mprimed
+echo -e '/var/log/mprime.log {\nrotate7\ndaily\nmissingok\nnotifempty\ncompress\ndelaycompress\ncopytruncate\n}' | sudo tee -a /etc/logrotate.d/mprimed
 
-# Setuo init.d daemon
-sudo cp ./mprime /etc/init.d/mprime
-sudo chmod a+x /etc/init.d/mprime
+# Setup init.d daemon
+sudo cp ./mprimed /etc/init.d/mprimed
+sudo chmod a+x /etc/init.d/mprimed
 # Run Daemon on boot
-sudo update-rc.d mprime defaults
+sudo update-rc.d mprimed defaults
 
 # Install MPrime
 wget http://www.mersenne.org/ftp_root/gimps/p95v293.linux64.tar.gz -P /tmp
